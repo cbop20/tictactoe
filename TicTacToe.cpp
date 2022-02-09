927358d089d2a6bb89714e093d65504e9e43eacf
dyst7453 <dyst7453@colorado.edu>
2022-02-09 14:50:28 -0700

    Implementation for DisplayBoard Person2

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,12 +6,29 @@ using namespace std;
 * Initialize empty matrix for new game
 */
 vector<vector<char>> CreateBoard(){
-   vector<vector<char>> board{{'','',''},{'','',''},{'','',''}};
+   vector<vector<char>> board{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
    return board; 
+}
+
+/**
+ *  Display the 3x3 matrix
+ */
+void DisplayBoard(vector<vector<char>> board){
+    cout << "-------------" << endl;
+    for(int i = 0; i < board.size(); i++){
+        cout << "| ";
+        for(int j = 0; j < board[i].size(); j++) {
+            cout << board[i][j] << " | ";
+        }
+        cout << endl;
+        cout << "-------------" << endl;
+    }
 }
 
 int main(){
     vector<vector<char>> board = CreateBoard();
+
+    DisplayBoard(board);
    
     return 0;
 }
