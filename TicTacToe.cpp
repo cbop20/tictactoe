7b36bbe28a2869d0b2f5807841ae33d6bbebd6f5
Caleb Starkey <cast7514@colorado.edu>
2022-02-09 13:11:01 -0700

    display board implementation

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,12 +6,20 @@ using namespace std;
 * Initialize empty matrix for new game
 */
 vector<vector<char>> CreateBoard(){
-   vector<vector<char>> board{{'','',''},{'','',''},{'','',''}};
+   vector<vector<char>> board{{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
    return board; 
+}
+
+void DisplayBoard(vector<vector<char>> b ){
+    for(int i = 0; i < 3; i ++){
+        for(int j = 0; j < 3; j++){
+            cout<<'|' << b[i][j] << '|';
+        }
+        cout << endl;
+    }
 }
 
 int main(){
     vector<vector<char>> board = CreateBoard();
-   
-    return 0;
+    DisplayBoard(board);
 }
